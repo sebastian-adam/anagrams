40e4fb719c7cf7f24708d51ef15dcb82784b5ac4
Noah Yasskin and Sebastian Prokuski <sebastian@>
2016-06-30 15:03:45 -0700

    add functional ui and capybara test

--- app.rb
+++ app.rb
@@ -1,14 +1,15 @@
-# require('sinatra')
-# require('sinatra/reloader')
-# require('./lib/find_replace')
-# also_reload('lib/**/*.rb')
-#
-# get('/') do
-#   erb(:index)
-# end
-#
-# get('/result') do
-#   @word = params.fetch('word')
-#   @string = params.fetch('string')
-#   @results = params.@word.anagrams(@string)
-# end
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/anagrams')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
+
+get('/results') do
+  @word = params.fetch('word')
+  @string = params.fetch('string')
+  @results = @string.anagram(@word)
+  erb(:results)
+end
--- spec/anagrams_integration_spec.rb
+++ spec/anagrams_integration_spec.rb
@@ -1,6 +1,6 @@
 require('capybara/rspec')
 require('./app')
-Capybars.app = Sinatra::Application
+Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the anagram detector path', {:type => :feature}) do
@@ -10,8 +10,16 @@ describe('the anagram detector path', {:type => :feature}) do
     fill_in('word', :with => 'dog')
     fill_in('string', :with => 'god')
     click_button('anagrammed')
-    expect(page).to have_content('true')
+    expect(page).to have_content('anagrams: god')
+    expect(page).to have_content('not anagrams: ')
   end
 
-
+  it('takes sentence from the user and another word and determines which words if any are anagrams of the word') do
+    visit('/')
+    fill_in('word', :with => 'dog')
+    fill_in('string', :with => 'god good gosh')
+    click_button('anagrammed')
+    expect(page).to have_content('anagrams: god')
+    expect(page).to have_content('not anagrams: good gosh')
+  end
 end
